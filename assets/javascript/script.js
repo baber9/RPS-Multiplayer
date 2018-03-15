81aaf5267354d14716bc3370589215df65171bca
Bryan Aber <baber9@gmail.com>
2018-03-15 14:11:11 -0500

    finished presence functionality for 2 players

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -36,4 +36,60 @@
 
   var database = firebase.database();
 
+  // chat-message
+  // display-message
+
+  // Runs when a change is made 
+  // database.ref().on("value", function(snapshot) {
+  //   $("#chat-message-log").append(snapshot.val().chatMessage + "\n");
+  // });
+
+  // $("#message-button").on("click", function(e) {
+  //   e.preventDefault();
+  //   var message = $("#my-message-input").val();
+  //     console.log(message);
+  //   database.ref().set({
+  //     chatMessage:message
+  //   });
+  // })
+  var playerCounter;  
+
+  var connectionsRef = database.ref("players/");
+  var connectedRef = database.ref(".info/connected");
+
+
+
+  connectionsRef.on("value", function(snap) {
+    playerCounter = snap.numChildren();
+    console.log(playerCounter);
   
+  });
+
+  // ADD PLAYER ONCLICK FUNCTION
+  
+
+  $("#player-name-submit").on("click", function(e) {
+    var name;
+    connectedRef.on("value", function(snap) {
+      if(snap.val()) {
+        e.preventDefault();
+        var playersRef = database.ref("players/" + (playerCounter + 1));
+        playersRef.onDisconnect().remove();
+      }
+    
+      if (playerCounter < 2) {
+        name = $("#player-name-input").val().trim();
+        playersRef.set({
+          name: name,
+          losses: 0,
+          wins: 0
+        });
+        $(".form-inline").hide();
+        $(".player-welcome").html(`Hi ${name}! You are Player ${playerCounter}`);
+      }
+    });
+
+
+
+  });
+
